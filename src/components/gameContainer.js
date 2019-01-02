04ba9d43a6a310703e502883d0f8242094b9fa44
=Michael Gingras <mcg79@cornell.edu>
2019-01-02 15:15:50 -0500

    live invites is working lol

--- src/components/gameContainer.js
+++ src/components/gameContainer.js
@@ -15,10 +15,11 @@ class GameContainer extends Component {
     this.state = {
       gameId: null,           // the id of the game to reference in firebase
       live: false,            // if there is a game currently joined
-      invite: 'michael',      // if you got an invite, who it is from
+      invite: null,      // if you got an invite, who it is from
     }
 
     this.searchLobby = this.searchLobby.bind(this);
+    this.searchGames = this.searchGames.bind(this);
   }
 
 
@@ -36,18 +37,47 @@ class GameContainer extends Component {
       console.log(games);
       if (uid in games){
         const oid = games[uid]
-        console.log(ctx.state.invite);
-        // ctx.setState({
-        //   invite: oid
-        // })
-        console.log("inivitation from " + oid);
+        ctx.setState({
+          invite: oid
+        })
       }
     });
   }
 
-  joinGame(){
+  searchGames(uid){
+    let ctx = this;
+    console.log("searching for games " + uid);
+    let games = firebase.database().ref('games/');
+    games.on('value', function(snapshot) {
+      const gameList = snapshot.val() || null;
+      console.log(gameList);
+      if (uid in gameList){
+        const oid = games[uid]
+        ctx.setState({
+          live: true
+        })
+        firebase.database().ref('games/' + uid).set(null);
+      }
+    });
+  }
+
+
+  // removes the game from the lobby (since it is now live)
+  joinGame(oid){
+    firebase.database().ref('lobby/' + this.props.global).set(null);
     this.setState({ live: true });
     this.setState({ gameId: 1 }); // how to communicate that to the other one?
+    firebase.database().ref('games/' + oid).set(this.props.global);
+
+    // add game to firebase
+  }
+
+
+  // rejects the game invite and removes it from the 'lobby' aka the firebase store
+  // also set the state of invite to null
+  rejectGame(){
+    firebase.database().ref('lobby/' + this.props.global).set(null);
+    this.setState({invite: null})
   }
 
   render() {
@@ -56,18 +86,20 @@ class GameContainer extends Component {
       <div>
         { !isLive ? (
           <div>
+            {(this.props.global && this.state.invite == null) && this.searchLobby(this.props.global)}
+            {(this.props.global && this.state.live == false) && this.searchGames(this.props.global)}
             {this.state.invite &&
                <InviteModal
                   opponent={this.state.invite}
                   bodyText="New challenge request from "
                   acceptText="Accept"
                   declineText="Decline"
-                  onAccept={() => {this.joinGame()}}
+                  onAccept={() => {this.joinGame(this.state.invite)}}
+                  onReject={() => {this.rejectGame()}}
                 />
             }
             <Header title="Player Select" path="/" right="add" />
             <PlayerPicker uid={this.props.global} />
-            {this.props.global && this.searchLobby(this.props.global)}
             <button className="button" onClick={ () => this.setState({live: true}) }>Start Game</button>
           </div>
         ) : (
